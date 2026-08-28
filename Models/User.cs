f20f3d00676b31c6ec231becd9732fcd92a64db6
Harshbhatt <113496500+harshbhatt2106@users.noreply.github.com>
2026-08-28 16:58:54 +0530

    Add user model matching CRUD API

---
+++ Models/User.cs
@@ -0,0 +1,9 @@
+namespace MobileAppTest.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public int Age { get; set; }
+}
