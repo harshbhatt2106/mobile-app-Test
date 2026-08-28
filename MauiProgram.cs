9edd280bcc2595653158c91302e5f1a617a7324c
Harshbhatt <113496500+harshbhatt2106@users.noreply.github.com>
2026-08-28 17:00:31 +0530

    Remove unavailable template font dependencies

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -9,12 +9,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
 
         builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            .UseMauiApp<App>();
 
 #if DEBUG
         builder.Logging.AddDebug();
